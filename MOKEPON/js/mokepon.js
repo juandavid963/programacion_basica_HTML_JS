a9117181832242592269d6dd24a1a8617d1ce5f3
Juan Cerquera <juandavid963@hotmail.com>
2023-01-06 14:46:57 -0500

    Agrego lógica para seleccionar el botón

--- MOKEPON/js/mokepon.js
+++ MOKEPON/js/mokepon.js
@@ -1,6 +1,24 @@
 function seleccionarMascotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA')
+ 
+    if(document.getElementById('hipodoge').checked){
+        alert('SELECCIONASTE TU MASCOTA HIPODOGE')
+    }
+    else if(document.getElementById('capipepo').checked){
+        alert('SELECCIONASTE TU MASCOTA CAPIPEPO')
+    }
+    else if(document.getElementById('ratigueya').checked){
+        alert('SELECCIONASTE TU MASCOTA RATIGUEYA')
+    }
+    else{
+        alert('NO SELECCIONASTE TU MASCOTA')
+    }
+    
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+//para que todos los elementos de HTML existan
+window.addEventListener('load', iniciarJuego)
